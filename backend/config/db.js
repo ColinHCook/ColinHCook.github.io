0a742784d156f4cc9e27ff5afa58755b2d8e0e23
ColinHCook <chc5197@psu.edu>
2024-08-18 15:30:47 -0400

    fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,20 @@
-const { Client } = require("pg");
-const client = new Client({
+const { Pool } = require("pg");
+
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false, // This ensures SSL is used without rejecting self-signed certificates
   },
 });
 
-client.connect();
+pool.connect();
 
-client.query("SET search_path TO public;", (err, res) => {
+pool.query("SET search_path TO public;", (err, res) => {
   if (err) {
     console.error("Error setting search path:", err.stack);
   } else {
     console.log("Search path set to public");
   }
 });
+
+module.exports = pool;
